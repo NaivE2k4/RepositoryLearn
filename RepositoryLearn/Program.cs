898be6b44b1eb79d1c30c593455293ad79f3d28f
Anton Chebotarev <ferdinand2k4@gmail.com>
2022-06-07 00:36:31 +0300

    initial

--- RepositoryLearn/Program.cs
+++ RepositoryLearn/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ Это просто проект для практики разных базовых штук по работе с данными
+ */
+
+using RepositoryLearn;
+
+UnitOfWork a = new();
---
+++ RepositoryLearn/UnitOfWork.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLearn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLearn
+{
+    public class UnitOfWork : IDisposable
+    {
+        private DbContext _dBContext = new DbContext(new DbContextOptionsBuilder().Options);
+        private IGenericRepository<Company> companies;
+        private IGenericRepository<Phone> phones;
+
+        public IGenericRepository<Company> Companies
+        { 
+            get 
+            {
+                if (companies == null)
+                    companies = new EFGenericRepository<Company>(_dBContext);
+                return companies; 
+            }
+        }
+        public IGenericRepository<Phone> Phones
+        {
+            get
+            {
+                if(phones == null)
+                    phones = new EFGenericRepository<Phone>(_dBContext);
+                return phones;
+            }
+        }
+
+        public void Save()
+        { 
+            _dBContext.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _dBContext.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
