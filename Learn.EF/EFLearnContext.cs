4d9010a755fdb8e0696c6c2363a6b53991c5bf0a
Anton Chebotarev <ferdinand2k4@gmail.com>
2022-06-08 00:34:20 +0300

    In progress

--- Learn.EF/EFLearnContext.cs
+++ Learn.EF/EFLearnContext.cs
@@ -8,6 +8,18 @@ namespace Learn.EF
         public DbSet<Company> Companies { get; set; }
         public DbSet<Phone> Phones { get; set; }
 
+        public string DbPath { get; }
 
+        public EFLearnContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
     }
 }
--- RepositoryLearn/Program.cs
+++ RepositoryLearn/Program.cs
@@ -2,6 +2,7 @@
  Это просто проект для практики разных базовых штук по работе с данными
  */
 
+using Learn.EF;
 using RepositoryLearn;
 
-UnitOfWork a = new();
+//UnitOfWork<EFLearnContext> a = new();
