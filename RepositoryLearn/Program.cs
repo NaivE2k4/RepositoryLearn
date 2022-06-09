173379dec2c8d3793d59bb26f9e82f942f1af590
Anton Chebotarev <ferdinand2k4@gmail.com>
2022-06-09 18:24:12 +0300

    test some code, project link

--- RepositoryLearn/Program.cs
+++ RepositoryLearn/Program.cs
@@ -8,10 +8,35 @@ https://www.youtube.com/watch?v=oP_OUiIK4Rc
 https://stackoverflow.com/questions/42957140/repository-design-pattern-with-dapper
  */
 
+using Learn.Dapper;
 using Learn.EF;
-using RepositoryLearn.Models;
+
 
 var efContext = new EFLearnContext();
-EFUnitOfWork<EFLearnContext> a = new(efContext);
-a.Companies.Create(new Company { Id = 1, Name = "First" });
-a.Save();
+using (EFUnitOfWork<EFLearnContext> uow = new(efContext))
+{
+    //a.Companies.Create(new Company { Id = 1, Name = "First" });
+    //a.Save();
+    foreach (var item in await uow.Companies.GetAsync())
+    {
+        Console.WriteLine($"{item.Id}: {item.Name}");
+    }
+
+    var item1 = uow.Companies.FindById(1);
+    item1.Name = item1.Name + "1";
+    uow.Companies.Update(item1);
+    foreach (var item in await uow.Companies.GetAsync())
+    {
+        Console.WriteLine($"{item.Id}: {item.Name}");
+    }
+    uow.Save();
+    foreach (var item in await uow.Companies.GetAsync())
+    {
+        Console.WriteLine($"{item.Id}: {item.Name}");
+    }
+}
+var dapperCompanyRepo = new DapperCompanyRepository();
+foreach (var item in await dapperCompanyRepo.GetAsync())
+{
+    Console.WriteLine($"{item.Id}: {item.Name}");
+}
