4271e5d0dcd4fddc9c3be89bdaa7904c31dc184b
achebotarev <achebotarev@kraftway.ru>
2022-06-10 15:55:03 +0300

    Working on nhiber repo

--- Learn.NHibernate/NHibernateCompanyRepository.cs
+++ Learn.NHibernate/NHibernateCompanyRepository.cs
@@ -11,13 +11,37 @@ public class NHibernateCompanyRepository : IGenericRepository<Company>
     {
         _session = session;
     }
-    public void Create(Company item) => throw new NotImplementedException();
-    public Task CreateAsync(Company item) => throw new NotImplementedException();
-    public Company FindById(int id) => throw new NotImplementedException();
-    public Task<Company> FindByIdAsync(int id) => throw new NotImplementedException();
-    public IEnumerable<Company> Get() => throw new NotImplementedException();
-    public IEnumerable<Company> Get(Func<Company, bool> predicate) => throw new NotImplementedException();
-    public Task<IEnumerable<Company>> GetAsync() => throw new NotImplementedException();
+    public void Create(Company item)
+    { 
+        _session.Save(item);
+    }
+    public async Task CreateAsync(Company item)
+    {
+        await _session.SaveAsync(item);
+    }
+    public Company FindById(int id)
+    {
+        return _session.Get<Company>(id);
+    }
+    public async Task<Company> FindByIdAsync(int id)
+    {
+        return await _session.GetAsync<Company>(id);
+    }
+
+    public IEnumerable<Company> Get()
+    {
+        return _session.Query<Company>().ToList();
+    }
+
+    public IEnumerable<Company> Get(Func<Company, bool> predicate)
+    { 
+        return _session.Query<Company>().Where(predicate).ToList();
+    }
+    public async Task<IEnumerable<Company>> GetAsync()
+    {
+        var result =_session.Query<Company>().ToList();
+        return await Task.FromResult(result);
+    }
     public Task<IEnumerable<Company>> GetAsync(Func<Company, bool> predicate) => throw new NotImplementedException();
     public void Remove(Company item) => throw new NotImplementedException();
     public Task RemoveAsync(Company item) => throw new NotImplementedException();
